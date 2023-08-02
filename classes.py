556d2c1d6ff03127b6bac6542b2bb723d2c0e2ba
stepan091991 <stepan.fedorov00@mail.ru>
2023-08-02 19:57:08 +0500

    10+ обновлений, фикс 5 багов, боты с ии, усложнение по мере игры

--- classes.py
+++ classes.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-
+import random
 left_image = pygame.image.load("textures/entity/bullet_4.png")
 right_image = pygame.image.load("textures/entity/bullet_2.png")
 up_image = pygame.image.load("textures/entity/bullet_1.png")
@@ -17,14 +17,13 @@ class Block(pygame.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         self.block_type = block_type
-        self.left_rect = (self.rect.x, self.rect.y, 5, 64)
-        self.right_rect = (self.rect.x + 59, self.rect.y, 5, 64)
-        self.up_rect = (self.rect.x, self.rect.y, 64, 5)
-        self.down_rect = (self.rect.x, self.rect.y + 59, 64, 5)
+        self.left_rect = (self.rect.x, self.rect.y, 1, 64)
+        self.right_rect = (self.rect.x + 63, self.rect.y, 1, 64)
+        self.up_rect = (self.rect.x, self.rect.y, 64, 1)
+        self.down_rect = (self.rect.x, self.rect.y + 63, 64, 1)
         self.width = 64
         self.height = 64
-        if self.block_type == 11:
-            print(self.rect)
+        self.spawn_collide = []
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, texture_up, texture_up_1, texture_left, texture_left_1, texture_down, texture_down_1,
@@ -59,11 +58,16 @@ class Player(pygame.sprite.Sprite):
         self.bullet = None
         self.last = 0
         self.cooldown = 150
+        self.now = 0
+        self.entity_type = "player"
+        self.health = 3
+        self.killed_coint = 0
+        self.level = 1
 
-    def update(self, blocks):
-        now = pygame.time.get_ticks()
-        if now - self.last >= self.cooldown:
-            self.last = now
+    def update(self, blocks, entitys):
+        self.now = pygame.time.get_ticks()
+        if self.now - self.last >= self.cooldown:
+            self.last = self.now
             self.animation()
         collide_fase = []
         keystate = pygame.key.get_pressed()
@@ -74,7 +78,7 @@ class Player(pygame.sprite.Sprite):
                     break
                 else:
                     self.speed = 2
-            if block.block_type != 0 and block.block_type != 8:
+            if block.block_type != 0 and block.block_type != 8 and block.block_type != 13:
                 if block.rect.colliderect(self.left_rect):
                     collide_fase.append("left")
                 if block.rect.colliderect(self.right_rect):
@@ -82,6 +86,16 @@ class Player(pygame.sprite.Sprite):
                 if block.rect.colliderect(self.up_rect):
                     collide_fase.append("up")
                 if block.rect.colliderect(self.down_rect):
+                    collide_fase.append("down")
+        for entity in entitys:
+            if entity != self:
+                if entity.rect.colliderect(self.left_rect):
+                    collide_fase.append("left")
+                if entity.rect.colliderect(self.right_rect):
+                    collide_fase.append("right")
+                if entity.rect.colliderect(self.up_rect):
+                    collide_fase.append("up")
+                if entity.rect.colliderect(self.down_rect):
                     collide_fase.append("down")
         if keystate[pygame.K_LEFT] and self.rect.x > 0 and not "left" in collide_fase:
             if self.image != self.texture_left:
@@ -116,22 +130,22 @@ class Player(pygame.sprite.Sprite):
     def spawn_bullet(self):
         if not self.bullet:
             if self.image == self.texture_left or self.image == self.texture_left_1:
-                self.bullet = Bullet(self, self.rect.x, self.rect.y + 24)
+                self.bullet = Bullet(self, self.rect.x + 10, self.rect.y + 24)
             if self.image == self.texture_right or self.image == self.texture_right_1:
                 self.bullet = Bullet(self, self.rect.x + 40, self.rect.y + 24)
             if self.image == self.texture_down or self.image == self.texture_down_1:
                 self.bullet = Bullet(self, self.rect.x + 23, self.rect.y + 40)
             if self.image == self.texture_up or self.image == self.texture_up_1:
-                self.bullet = Bullet(self, self.rect.x + 3, self.rect.y)
+                self.bullet = Bullet(self, self.rect.x + 3, self.rect.y + 10)
         if self.bullet.rect.x > 716 or self.bullet.rect.y > 780 or self.bullet.rect.x < 0 or self.bullet.rect.y < 0:
             if self.image == self.texture_left or self.image == self.texture_left_1:
-                self.bullet = Bullet(self, self.rect.x, self.rect.y + 24)
+                self.bullet = Bullet(self, self.rect.x + 10, self.rect.y + 24)
             if self.image == self.texture_right or self.image == self.texture_right_1:
                 self.bullet = Bullet(self, self.rect.x + 40, self.rect.y + 24)
             if self.image == self.texture_down or self.image == self.texture_down_1:
                 self.bullet = Bullet(self, self.rect.x + 23, self.rect.y + 40)
             if self.image == self.texture_up or self.image == self.texture_up_1:
-                self.bullet = Bullet(self, self.rect.x + 3, self.rect.y)
+                self.bullet = Bullet(self, self.rect.x + 3, self.rect.y + 10)
     def animation(self):
         if self.image == self.texture_left:
             self.image = self.texture_left_1
@@ -149,6 +163,8 @@ class Player(pygame.sprite.Sprite):
             self.image = self.texture_down_1
         elif self.image == self.texture_down_1:
             self.image = self.texture_down
+    def damaget(self,all_sprites,Bots,Entitys):
+        self.health -= 1
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,owner,x,y):
@@ -191,7 +207,7 @@ class Bullet(pygame.sprite.Sprite):
             self.rect.center = (4 / 2, 6 / 2)
             self.rect.x = x
             self.rect.y = y
-    def update(self,blocks,bullets):
+    def update(self,blocks,bullets,Entitys,all_sprites,Bots):
         if self.orientation == "up" and self.orientation:
             self.rect.y -= self.speed
         if self.orientation == "left" and self.orientation:
@@ -209,48 +225,217 @@ class Bullet(pygame.sprite.Sprite):
                     block.image = block.image.subsurface((0, 0, block.width, block.height))
                     block.rect.width -= 16
                     block.image.set_colorkey((255,255,255))
-                    block.right_rect = (block.right_rect[0] - 16, block.rect.y, 5, block.height)
-                    block.up_rect = (block.rect.x, block.rect.y, block.width, 5)
-                    block.down_rect = (block.rect.x, block.rect.y + block.height - 5, block.width, 5)
+                    block.right_rect = (block.right_rect[0] - 16, block.rect.y, 1, block.height)
+                    block.up_rect = (block.rect.x, block.rect.y, block.width, 1)
+                    block.down_rect = (block.rect.x, block.rect.y + block.height - 1, block.width, 1)
                     bullets.remove(self)
-                    self.rect.x = 9999
-                    self.kill()
+                    bullets.remove(bullets)
+                    self.owner.bullet = None
                 if self.rect.colliderect(block.left_rect):
                     block.width -= 16
                     block.image = block.image.subsurface((0, 0, block.width, block.height))
                     block.rect.width -= 16
                     block.image.set_colorkey((255,255,255))
-                    block.left_rect = (block.left_rect[0] + 16, block.rect.y, 5, block.height)
-                    block.up_rect = (block.rect.x + 16, block.rect.y, block.width, 5)
-                    block.down_rect = (block.rect.x + 16, block.rect.y + block.height - 5, block.width, 5)
+                    block.left_rect = (block.left_rect[0] + 16, block.rect.y, 1, block.height)
+                    block.up_rect = (block.rect.x + 16, block.rect.y, block.width, 1)
+                    block.down_rect = (block.rect.x + 16, block.rect.y + block.height - 1, block.width, 1)
                     block.rect.x += 16
                     bullets.remove(self)
-                    self.rect.x = 9999
-                    self.kill()
+                    bullets.remove(bullets)
+                    self.owner.bullet = None
                 if self.rect.colliderect(block.down_rect):
                     block.height -= 16
                     block.rect.height -= 16
                     block.image = block.image.subsurface((0, 0, block.width, block.height))
                     block.image.set_colorkey((255,255,255))
-                    block.right_rect = (block.right_rect[0], block.right_rect[1], 5, block.height)
-                    block.left_rect = (block.left_rect[0], block.left_rect[1], 5, block.height)
-                    block.down_rect = (block.down_rect[0], block.down_rect[1] - 16, block.width, 5)
+                    block.right_rect = (block.right_rect[0], block.right_rect[1], 1, block.height)
+                    block.left_rect = (block.left_rect[0], block.left_rect[1], 1, block.height)
+                    block.down_rect = (block.down_rect[0], block.down_rect[1] - 16, block.width, 1)
                     bullets.remove(self)
-                    self.rect.x = 9999
-                    self.kill()
+                    bullets.remove(bullets)
+                    self.owner.bullet = None
                 if self.rect.colliderect(block.up_rect):
                     block.height -= 16
                     block.rect.height -= 16
                     block.image = block.image.subsurface((0, 0, block.width, block.height))
                     block.image.set_colorkey((255,255,255))
-                    block.right_rect = (block.right_rect[0], block.right_rect[1] + 16, 5, block.height)
-                    block.left_rect = (block.left_rect[0], block.left_rect[1] + 16, 5, block.height)
-                    block.up_rect = (block.rect.x, block.rect.y + 16, block.width, 5)
+                    block.right_rect = (block.right_rect[0], block.right_rect[1] + 16, 1, block.height)
+                    block.left_rect = (block.left_rect[0], block.left_rect[1] + 16, 1, block.height)
+                    block.up_rect = (block.rect.x, block.rect.y + 16, block.width, 1)
                     block.rect.y += 16
-                    self.rect.x = 9999
-                    self.kill()
+                    bullets.remove(bullets)
+                    self.owner.bullet = None
             elif block.block_type != 1 and block.block_type != 0 and block.block_type != 7 and block.block_type != 8 and block.block_type != 11:
                 if self.rect.colliderect(block.rect):
-                    bullets.remove(self)
-                    self.rect.x = 9999
-                    self.kill()
+                    bullets.remove(bullets)
+                    self.owner.bullet = None
+        for entity in Entitys:
+            if self.rect.colliderect(entity):
+                if self.owner != entity:
+                    if self.owner.entity_type != entity.entity_type:
+                        bullets.remove(bullets)
+                        self.owner.bullet = None
+                        if self.owner.entity_type == "player":
+                            self.owner.killed_coint += 1
+                        entity.damaget(all_sprites,Bots,Entitys)
+bot_texture_left = pygame.image.load("textures/entity/Enemy/2.png")
+bot_texture_right = pygame.image.load("textures/entity/Enemy/4.png")
+bot_texture_up = pygame.image.load("textures/entity/Enemy/1.png")
+bot_texture_down = pygame.image.load("textures/entity/Enemy/3.png")
+
+bot_texture_left_1 = pygame.image.load("textures/entity/Enemy/2_1.png")
+bot_texture_right_1 = pygame.image.load("textures/entity/Enemy/4_1.png")
+bot_texture_up_1 = pygame.image.load("textures/entity/Enemy/1_1.png")
+bot_texture_down_1 = pygame.image.load("textures/entity/Enemy/3_1.png")
+class Bot(pygame.sprite.Sprite):
+    def __init__(self, types, blocks,x,y):
+        pygame.sprite.Sprite.__init__(self)
+        self.entity_type = "enemy"
+        self.texture_left = bot_texture_left
+        self.texture_right = bot_texture_right
+        self.texture_up = bot_texture_up
+        self.texture_down = bot_texture_down
+        self.texture_left_1 = bot_texture_left_1
+        self.texture_right_1 = bot_texture_right_1
+        self.texture_up_1 = bot_texture_up_1
+        self.texture_down_1 = bot_texture_down_1
+        self.blocks = blocks
+        self.types = types
+        self.image = self.texture_down
+        self.rect = self.image.get_rect()
+        self.speed = 2
+        self.left_rect = (self.rect.x, self.rect.y,10,52)
+        self.right_rect = (self.rect.x + 42, self.rect.y,10,52)
+        self.up_rect = (self.rect.x, self.rect.y, 52, 10)
+        self.down_rect = (self.rect.x, self.rect.y + 42, 52, 52 / 4)
+        self.bullet = None
+        self.last = 0
+        self.last_1 = 0
+        self.shoot_cooldown = random.randint(300,700)
+        self.animation_cooldown = 150
+        self.rect.x = x
+        self.rect.y = y
+        self.now = 0
+        self.rotate = "up"
+        self.randim_id = random.randint(0,999)
+    def update(self,blocks,entitys):
+        self.now = pygame.time.get_ticks()
+        if self.now - self.last >= self.animation_cooldown:
+            self.last = self.now
+            self.animation()
+        if self.now - self.last_1 >= self.shoot_cooldown:
+            self.last_1 = self.now
+            self.shoot()
+        collide_fase = []
+        score = random.randint(1,1000)
+        if score == 0 or 1 < score < 998:
+            for block in blocks:
+                if self.rect.colliderect(block.rect):
+                    if block.block_type == 8:
+                        self.speed = 7
+                        break
+                    else:
+                        self.speed = 2
+                if block.block_type != 0 and block.block_type != 8 and block.block_type != 13:
+                    if block.rect.colliderect(self.left_rect):
+                        collide_fase.append("left")
+                    if block.rect.colliderect(self.right_rect):
+                        collide_fase.append("right")
+                    if block.rect.colliderect(self.up_rect):
+                        collide_fase.append("up")
+                    if block.rect.colliderect(self.down_rect):
+                        collide_fase.append("down")
+            for entity in entitys:
+                if entity != self:
+                    if entity.rect.colliderect(self.left_rect):
+                        collide_fase.append("left")
+                    if entity.rect.colliderect(self.right_rect):
+                        collide_fase.append("right")
+                    if entity.rect.colliderect(self.up_rect):
+                        collide_fase.append("up")
+                    if entity.rect.colliderect(self.down_rect):
+                        collide_fase.append("down")
+            if not "left" in collide_fase and self.rect.x > 0 and self.rotate == "left":
+                self.rect.x -= self.speed
+            elif not "right" in collide_fase and self.rect.x < 716 and self.rotate == "right":
+                self.rect.x += self.speed
+            elif not "up" in collide_fase and self.rect.y > 0 and self.rotate == "up":
+                self.rect.y -= self.speed
+            elif not "down" in collide_fase and self.rect.y < 780 and self.rotate == "down":
+                self.rect.y += self.speed
+            else:
+                score = random.randint(1,4)
+                if score == 1:
+                    self.rotate = "left"
+                    self.image = self.texture_left
+                if score == 2:
+                    self.rotate = "right"
+                    self.image = self.texture_right
+                if score == 3:
+                    self.rotate = "up"
+                    self.image = self.texture_up
+                if score == 4:
+                    self.rotate = "down"
+                    self.image = self.texture_down
+        else:
+            score = random.randint(1, 4)
+            if score == 1:
+                self.rotate = "left"
+                self.image = self.texture_left
+            if score == 2:
+                self.rotate = "right"
+                self.image = self.texture_right
+            if score == 3:
+                self.rotate = "up"
+                self.image = self.texture_up
+            if score == 4:
+                self.rotate = "down"
+                self.image = self.texture_down
+        collide_fase.clear()
+        self.left_rect = (self.rect.x, self.rect.y + 5, 10, 42)
+        self.right_rect = (self.rect.x + 42, self.rect.y + 5, 10, 42)
+        self.up_rect = (self.rect.x + 5, self.rect.y, 42, 10)
+        self.down_rect = (self.rect.x + 5, self.rect.y + 42, 42, 10)
+    def shoot(self):
+        if not self.bullet:
+            if self.image == self.texture_left or self.image == self.texture_left_1:
+                self.bullet = Bullet(self, self.rect.x + 10, self.rect.y + 24)
+            if self.image == self.texture_right or self.image == self.texture_right_1:
+                self.bullet = Bullet(self, self.rect.x + 40, self.rect.y + 24)
+            if self.image == self.texture_down or self.image == self.texture_down_1:
+                self.bullet = Bullet(self, self.rect.x + 23, self.rect.y + 40)
+            if self.image == self.texture_up or self.image == self.texture_up_1:
+                self.bullet = Bullet(self, self.rect.x + 3, self.rect.y + 10)
+        if self.bullet.rect.x > 716 or self.bullet.rect.y > 780 or self.bullet.rect.x < 0 or self.bullet.rect.y < 0:
+            if self.image == self.texture_left or self.image == self.texture_left_1:
+                self.bullet = Bullet(self, self.rect.x + 10, self.rect.y + 24)
+            if self.image == self.texture_right or self.image == self.texture_right_1:
+                self.bullet = Bullet(self, self.rect.x + 40, self.rect.y + 24)
+            if self.image == self.texture_down or self.image == self.texture_down_1:
+                self.bullet = Bullet(self, self.rect.x + 23, self.rect.y + 40)
+            if self.image == self.texture_up or self.image == self.texture_up_1:
+                self.bullet = Bullet(self, self.rect.x + 3, self.rect.y + 10)
+    def animation(self):
+        if self.image == self.texture_left:
+            self.image = self.texture_left_1
+        elif self.image == self.texture_left_1:
+            self.image = self.texture_left
+
+        if self.image == self.texture_right:
+            self.image = self.texture_right_1
+        elif self.image == self.texture_right_1:
+            self.image = self.texture_right
+
+        if self.image == self.texture_up:
+            self.image = self.texture_up_1
+        elif self.image == self.texture_up_1:
+            self.image = self.texture_up
+
+        if self.image == self.texture_down:
+            self.image = self.texture_down_1
+        elif self.image == self.texture_down_1:
+            self.image = self.texture_down
+    def damaget(self,all_sprites,Bots,Entitys):
+        self.remove(all_sprites)
+        Bots.remove(self)
+        Entitys.remove(self)
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import os
+import random
 import sys
 import pygame
 from pygame.locals import *
 import classes
 import map
 import load_textures
+Bot_spawn_coint = 2
+bots_off = 0
+Bots = []
+Entitys = []
+Spawns = []
 pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 15)
 fps = 60
 fpsClock = pygame.time.Clock()
 width, height = 768, 832
@@ -19,16 +27,55 @@ bullets = pygame.sprite.Group()
 leaves_block = pygame.sprite.Group()
 created_map = map.create_map()
 for block in created_map:
+    if block.block_type == 13:
+        Spawns.append(block)
+for block in created_map:
     if block.block_type == 7:
         leaves_block.add(block)
     else:
         blocks.add(block)
 Player = load_textures.load_player()
+spawn_collide = []
+for i in range(Bot_spawn_coint):
+    spawn = random.choice(Spawns)
+    for bot in Bots:
+        if spawn.rect.colliderect(bot.rect):
+            spawn.spawn_collide.append("bot")
+    if not spawn.spawn_collide:
+        Bot = classes.Bot("type", blocks, spawn.rect.x, spawn.rect.y)
+        all_sprites.add(Bot)
+        Entitys.append(Bot)
+        Bots.append(Bot)
+Entitys.append(Player)
 all_sprites.add(Player)
 # Game loop.
 while True:
+    if len(Bots) == 0 and bots_off == 0:
+        Player.level += 1
+        if Player.level < 10:
+            Bot_spawn_coint += 1
+        elif Player.level >= 10 < 20:
+            Bot_spawn_coint += 2
+        elif Player.level >= 20:
+            Bot_spawn_coint += 3
+        bots_off = Bot_spawn_coint
+    if bots_off != 0:
+        spawn = random.choice(Spawns)
+        for bot in Bots:
+            if spawn.rect.colliderect(bot.rect):
+                spawn.spawn_collide.append("bot")
+        if not spawn.spawn_collide:
+            Bot = classes.Bot("type", blocks, spawn.rect.x, spawn.rect.y)
+            all_sprites.add(Bot)
+            Entitys.append(Bot)
+            Bots.append(Bot)
+            bots_off -= 1
+        spawn.spawn_collide = []
     if Player.bullet is not None:
         bullets.add(Player.bullet)
+    for bot in Bots:
+        if bot.bullet is not None:
+            bullets.add(bot.bullet)
     screen.fill((0, 0, 0))
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -37,7 +84,7 @@ while True:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 Player.spawn_bullet()
-    Player.update(blocks)
+    Player.update(blocks,Entitys)
     blocks.draw(screen)
     all_sprites.draw(screen)
     pygame.draw.rect(screen,WHITE,Player.rect,1)
@@ -45,17 +92,42 @@ while True:
     pygame.draw.rect(screen, LIGHT_BLUE, Player.right_rect, 2)
     pygame.draw.rect(screen, LIGHT_BLUE, Player.up_rect, 2)
     pygame.draw.rect(screen, LIGHT_BLUE, Player.down_rect, 2)
-    bullets.update(blocks,bullets)
+    bullets.update(blocks,bullets,Entitys,all_sprites,Bots)
     bullets.draw(screen)
     leaves_block.update()
     leaves_block.draw(screen)
     for block in blocks:
         if block.block_type != 0 and block.block_type != 7:
-            pygame.draw.rect(screen, LIGHT_BLUE, block.rect, 1)
-            pygame.draw.rect(screen, WHITE, block.left_rect, 1)
-            pygame.draw.rect(screen, WHITE, block.right_rect, 1)
-            pygame.draw.rect(screen, WHITE, block.up_rect, 1)
-            pygame.draw.rect(screen, WHITE, block.down_rect, 1)
+            #pygame.draw.rect(screen, LIGHT_BLUE, block.rect, 1)
+            #pygame.draw.rect(screen, WHITE, block.left_rect, 1)
+            #pygame.draw.rect(screen, WHITE, block.right_rect, 1)
+            #pygame.draw.rect(screen, WHITE, block.up_rect, 1)
+            #pygame.draw.rect(screen, WHITE, block.down_rect, 1)
             pass
+    for bot in Bots:
+        #pygame.draw.rect(screen, WHITE, bot.rect, 1)
+        #pygame.draw.rect(screen, LIGHT_BLUE, bot.left_rect, 2)
+        #pygame.draw.rect(screen, LIGHT_BLUE, bot.right_rect, 2)
+        #pygame.draw.rect(screen, LIGHT_BLUE, bot.up_rect, 2)
+        #pygame.draw.rect(screen, LIGHT_BLUE, bot.down_rect, 2)
+        bot.update(blocks, Entitys)
+    text_surface = my_font.render(f"Bullets:{len(bullets)} Enemys:{len(Bots)} Objects:{len(all_sprites) + len(blocks) + len(leaves_block)} ",False, (255, 255, 255))
+    screen.blit(text_surface, (0, 0))
+    text_surface = my_font.render(
+        f"Fps:{fpsClock}", False, (255, 255, 255))
+    screen.blit(text_surface, (0, 16))
+    text_surface = my_font.render(
+        f"Health:{Player.health} Killed:{Player.killed_coint} Level:{Player.level}", False, (255, 255, 255))
+    screen.blit(text_surface, (0, 32))
+    text_surface = my_font.render(
+        f"Spawns:{len(Spawns)} Bots_out:{bots_off}", False, (255, 255, 255))
+    screen.blit(text_surface, (0, 48))
+    text_surface = my_font.render(
+        f"Spawn1_info:{Spawns[0].spawn_collide}", False, (255, 255, 255))
+    screen.blit(text_surface, (0, 64))
+    text_surface = my_font.render(
+        f"Spawn2_info:{Spawns[1].spawn_collide}", False, (255, 255, 255))
+    screen.blit(text_surface, (0, 80))
     pygame.display.flip()
     fpsClock.tick(fps)
+
